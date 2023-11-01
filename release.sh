703b488717a48e7757202a6cfa498ad32f33bb01
Jürgen Seitz <juergen.seitz@h-d-gmbh.de>
2023-11-01 08:40:59 +0100

    build: prepare release v0.0.1

--- release.sh
+++ release.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 cargo set-version --workspace $1
-
+cargo build
 git add .
 git commit -m "build: prepare release v$1"
 git push
 # git tag v$1
 # git push -u origin v$1
 
-pnpm publish --recursive --access public --no-git-checks
-cargo publish -p wfrs-model
-cargo publish -p wfrs-validator
-cargo publish -p wfrs-engine
+cargo publish -p keycloak-access
